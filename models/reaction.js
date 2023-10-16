5ccf6f94742d72b9f879f30d0e1430774b7bc2aa
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-15 20:22:17 -0400

    thought and reaction schema

---
+++ models/reaction.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
