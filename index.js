5ce529e1964702e1163e44a7b37cdd6f10374514
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-15 20:16:53 -0400

    user schema created

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server for Social Network running on port ${PORT}!`);
-    });
+  app.listen(PORT, () => {
+    console.log(`API Server functional, running on port ${PORT}!`);
   });
-
+});
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please enter a valid email address']
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
